fea697288a36a5dc1320dbf65797d1b0d9c1228d
Sreenu Giri KP <148510216+Sr2006na@users.noreply.github.com>
2026-01-14 21:22:45 +0530

    calculator to a menu-driven CLI interface
    still some issue i need to solve it

--- calculator.c
+++ calculator.c
@@ -1,55 +1,106 @@
 #include <stdio.h>
 int main(){
-int choice;
-float num1,num2,result;
-char op;
+int choice,i,n;
+double num,result;
 
 
 while(1){
-printf("//SIMPLE CALCULATOR//\n");
-printf("Enter the operation ( + , - , * , / , 0-exit ): ");
-scanf(" %c",&op);
-
-if(op=='0'){
-    printf("Exit the calculator thanks for using!\n");
-    break;}
-else{    
-printf("Enter the first number: ");
-scanf("%f",&num1);
-printf("Enter the second number: ");
-scanf("%f",&num2);
-switch(op){
-    case('+'):
-    result=num1+num2;
-    printf("Addition:%.1f + %.1f = %.1f\n",num1,num2,result);
-    break;
-    case('-'):
-    result=num1-num2;
-    printf("substration:%.1f - %.1f = %.1f\n",num1,num2,result);
-    break;
-    case('*'):
-    result=num1*num2;
-    printf("multiplication:%.1f * %.1f = %.1f\n",num1,num2,result);
-    break;
-    case('/'):
-    if(num2!=0){
-    result=num1/num2;
-    printf("Divide:%.1f / %.1f = %.1f\n",num1,num2,result);
-    }else{
-    printf("ERROR divide by zero\n");
-    }
-    break;
-    default:
-    printf("Invalid Opertation!\n");
-  }
-}
-
-printf("Do you want to perform another calculation? (1 for Yes / 0 for No):");
+printf("\n");
+printf("******************************\n");
+printf("//Menu-driven CLI calculator//\n");
+printf("******************************\n");
+printf("\n1.Addition\n");
+printf("2.Subtraction\n");
+printf("3.Multiplication\n");
+printf("4.Division\n");
+printf("5.To exit the program\n");
+printf("\nEnter the option: ");
 scanf("%d",&choice);
-if (choice==0){
-printf("Exit the calculator thanks!\n");
+
+
+if(choice==5){
+printf("Exit the calculator thanks for using!\n");
 break;
 }
+switch(choice){
+
+case 1: 
+printf("\nAddition\n");
+printf("--------");
+result=0;
+printf("\nEnter the total number you are going to add: ");
+scanf("%d",&n);
+printf("Enter %d number and pressing enter after each number: \n", n);
+
+for(i=0;i<n;i++){
+scanf("%lf",&num);
+result += num;
 }
-  return 0;
+
+printf("\nSum = %lf\n",result);
+break;
+
+case 2: 
+printf("\nSubtraction\n");
+printf("-----------");
+printf("\nEnter the total number you are going to substrate: ");
+scanf("%d",&n);
+printf("Enter %d number and pressing enter after each number: \n", n);
+scanf("%lf",&result);
+
+for(i=1;i<n;i++){
+scanf("%lf",&num);
+result -= num;
 }
+
+printf("\nDifference = %lf\n",result);
+break;
+
+case 3:
+printf("\nMultiplication\n");
+printf("--------------");
+result=1;
+printf("\nEnter the total number you are going to multiple: ");
+scanf("%d",&n);
+printf("Enter %d number and pressing enter after each number: \n", n);
+for(i=0;i<n;i++){
+scanf("%lf",&num);
+result *= num;
+}
+
+printf("\nProduct: %lf\n",result);
+break;
+
+
+case 4:
+printf("\nDivision\n");
+printf("--------");
+printf("\nEnter the total number you are going to divide: ");
+scanf("%d",&n);
+printf("Enter %d number and pressing enter after each number: \n", n);
+scanf("%lf",&result);
+
+for(i=1;i<n;i++){
+scanf("%lf",&num);
+if (num ==0) {
+printf("\nError: Division by zero\n");
+break;
+}
+result /= num;
+} 
+if (num!=0){
+printf("\nDivision: %lf\n",result);
+}
+break;
+
+default:
+printf("Invalid option Please select between 1 and 5\n");
+
+}
+}
+
+return 0;
+}
+
+
+
